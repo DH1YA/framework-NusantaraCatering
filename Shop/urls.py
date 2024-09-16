cecc42452fcf133a57e6e1bb2b9d74a2145e229b
DH1YA <dhiya334@gmail.com>
2024-09-17 01:45:07 +0800

    kerangka-2

--- Shop/urls.py
+++ Shop/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from Shop import views
 
 urlpatterns = [
-  path('Shop/', views.index)
+  path('Shop', views.index),
+  path('Contact', views.contact)
+  
 ]
--- Shop/views.py
+++ Shop/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
   return render(request, 'shop/index.html')
+
+def contact(request):
+  return render(request, 'shop/contact.html')
